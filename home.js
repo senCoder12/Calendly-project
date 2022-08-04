445b944b5196ded34cab3565d454e3e76832b3d4
senCoder12 <mayukhsen85082@gmail.com>
2022-08-04 17:35:07 +0530

    mayukh contribution

--- home.js
+++ home.js
@@ -2,6 +2,15 @@
 var credentials = JSON.parse(localStorage.getItem("credentials"));  
 
 window.onload = function(){
+    // var nameEmail = JSON.parse(localStorage.getItem("nameEmail"));
+    // var fullName = nameEmail.name;
+    // var fullEmail = nameEmail.email;
+    // var getMail = fullEmail.split("@")[0];
+    // document.getElementById("nameFirstLater").innerText = fullName[0].toUpperCase; 
+    // document.getElementById("firstLa").innerText = fullName[0].toUpperCase; 
+    // document.getElementById("name").innerText = fullName;
+    // document.querySelector("#email").innerText = `calendly.com/${getMail}`;
+
     if(!localStorage.getItem("credentials") || JSON.parse(localStorage.getItem("credentials")).length == 0) {
         var obj =  {Ename : "30 Minute Meeting" , location : null , type : "One-on-One"};
         var cred = [];
@@ -24,7 +33,7 @@ function displayMeeting(credentials) {
         var createHtml = document.createElement("div");
         createHtml.innerHTML = 
         `<div>
-            <input type="checkbox" name="" id="checking" onclick="deleteMeeting(${index})">
+            <input type="checkbox" name="" class="checking" onclick="deleteMeeting(${index})">
             <i class="fa fa-gear"></i>
         </div>
         <h2>${elm.Ename}</h2>
@@ -43,7 +52,6 @@ document.querySelector(".popupBtn").addEventListener("click",showPopup);
 document.getElementById("popup").style.display = "none";
 function showPopup() {
     event.preventDefault();
-    console.log("hifi")
     if(document.getElementById("popup").style.display=="block") {
         document.getElementById("popup").style.display = "none";
     }else {
@@ -52,12 +60,25 @@ function showPopup() {
 }
 
 function deleteMeeting(i) {
-    if(document.querySelector(".section-7").style.display == "flex"){
+    var addBorder = event.target.parentNode.parentNode;
+    var opacityLess = event.target.parentNode.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling;
+
+    if(document.querySelectorAll(".checking")[i].checked == false){
         document.querySelector(".section-7").style.display = "none"
+        addBorder.removeAttribute("class");
+        opacityLess.removeAttribute("class");
     }else {
+        addBorder.setAttribute("class","border-blue");
+        opacityLess.setAttribute("class","opacityLess");
         document.querySelector(".section-7").style.display = "flex"
         document.getElementById("deleteIcon").addEventListener("click",function(){
             deleteNode(i);
+        });
+        document.querySelector(".adjust").addEventListener("click",function(){
+            document.querySelector(".section-7").style.display = "none";
+            document.querySelectorAll(".checking")[i].checked = false;
+            addBorder.removeAttribute("class");
+            opacityLess.removeAttribute("class");
         });
     }
 }
@@ -69,3 +90,20 @@ function deleteNode(i) {
     document.querySelector(".section-7").style.display = "none"
 }
 
+var to = null;
+$("#abc").on("keyup",function() {
+    if(to !== null) clearTimeout(to);
+    to = setTimeout(function(){
+        filterMeeting();
+       to = null;
+    }, 1000);
+})
+function filterMeeting() {
+    var filterName = document.getElementById("abc").value;
+    console.log(filterName)
+    var filterArr = credentials.filter(function(elm) {
+        return elm.Ename == filterName || elm.type == filterName;
+    })
+    displayMeeting(filterArr);
+    if(filterName=="") displayMeeting(credentials);
+}
