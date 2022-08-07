e2470c647672fca051377228458e289a07d33932
Pranesh Nath <pranesh.nathyadav.52@gmail.com>
2022-08-07 15:15:26 +0530

    changes

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-window.addEventListener("scroll",reveal);
+window.addEventListener("scroll", reveal);
 
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
+  var reveals = document.querySelectorAll(".reveal");
 
-    for(var i=0;i<reveals.length;i++){
-        var windowheight = window.innerHeight;
-        var revealtop = reveals[i].getBoundingClientRect().top;
-        var revealPoint = 150;
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
 
-        if(revealtop < windowheight - revealPoint){
-            reveals[i].classList.add("active");
-        }else {
-            reveals[i].classList.remove("active");
-        }
+    if (revealtop < windowheight - revealPoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
+  }
+}
+
+document.getElementById("proSet").addEventListener("click", design);
+var pro = document.getElementById("container");
+function design() {
+  if (pro.style.display == "none") {
+    pro.style.display = "flex";
+  } else {
+    pro.style.display = "none";
+  }
 }
